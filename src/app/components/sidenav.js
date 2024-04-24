623c8b6a9a86501e9bfbdcf9b4f93ba5b04f8409
Your Name <you@example.com>
2024-04-24 12:23:22 +0800

    fix the sidenav minor issue

--- src/app/components/sidenav.js
+++ src/app/components/sidenav.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import { FaBars, FaHome, FaInfoCircle, FaEnvelope, FaSignOutAlt } from 'react-icons/fa'; // Import icons
-import './sideNav.css';
+import "./sidenav.css";
 
 const SideNav = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -13,39 +13,41 @@ const SideNav = () => {
   const collapsedWidth = 40; // Width of the hamburger icon plus padding
 
   return (
-    <div className={`sidenav ${isNavOpen ? 'open' : ''}`} style={{ width: isNavOpen ? '250px' : `${collapsedWidth}px` }}>
-      <button className="toggle-btn" onClick={toggleNav}>
-        <FaBars />
-      </button>
-      <ul>
-        {isNavOpen && (
-          <>
-            <li><FaHome /> Home</li>
-            <li><FaInfoCircle /> About</li>
-            <li><FaEnvelope /> Contact</li>
-          </>
-        )}
-        {!isNavOpen && (
-          <>
-            <li><FaHome /></li>
-            <li><FaInfoCircle /></li>
-            <li><FaEnvelope /></li>
-          </>
-        )}
-      </ul>
-      <ul>
-      {isNavOpen && (
-          <>
-            <li><FaSignOutAlt /> Log out</li>
-          </>
-        )}
-        {!isNavOpen && (
-          <>
-            <li><FaSignOutAlt /></li>
-          </>
-        )}
-      </ul>
-    </div>
+    <>
+      <div className={`sidenav ${isNavOpen ? 'open' : ''}`} style={{ width: isNavOpen ? '250px' : `${collapsedWidth}px` }}>
+        <button className="toggle-btn" onClick={toggleNav}>
+          <FaBars />
+        </button>
+        <ul>
+          {isNavOpen && (
+            <>
+              <li><FaHome /> Home</li>
+              <li><FaInfoCircle /> About</li>
+              <li><FaEnvelope /> Contact</li>
+            </>
+          )}
+          {!isNavOpen && (
+            <>
+              <li><FaHome /></li>
+              <li><FaInfoCircle /></li>
+              <li><FaEnvelope /></li>
+            </>
+          )}
+        </ul>
+        <ul>
+          {isNavOpen && (
+            <>
+              <li><FaSignOutAlt /> Log out</li>
+            </>
+          )}
+          {!isNavOpen && (
+            <>
+              <li><FaSignOutAlt /></li>
+            </>
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
