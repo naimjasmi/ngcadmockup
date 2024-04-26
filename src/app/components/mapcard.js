51fa109d922ce4f7da9e8792f7ecad103a1a0883
Your Name <you@example.com>
2024-04-26 12:06:09 +0800

    add dummy marker

--- src/app/components/mapcard.js
+++ src/app/components/mapcard.js
@@ -1,8 +1,10 @@
-// mapcard.js
 import React, { useEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import styles from './mapcard.module.css'; // Correct import statement
+import 'leaflet/dist/leaflet.js';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import styles from './mapcard.module.css';
 
 const MapCard = () => {
     useEffect(() => {
@@ -12,10 +14,19 @@ const MapCard = () => {
             attribution: '&copy; OpenStreetMap contributors',
         }).addTo(map);
 
-        // Add a dummy marker
-        L.marker([2.9213, 101.6559]).addTo(map)
-            .bindPopup('Dummy Marker')
-            .openPopup();
+        // Create a custom div icon with React Icons (Font Awesome) and larger size
+        const icon = L.divIcon({
+            className: 'fa-icon',
+            html: ReactDOMServer.renderToString(<FaMapMarkerAlt size="2x" />), // Increase the size using "2x"
+            iconSize: [20, 20], // Increase the size of the icon container
+        });
+
+        // Add first dummy marker
+        L.marker([2.9213, 101.6559], { icon: icon }).addTo(map);
+
+        // Add more dummy markers
+        L.marker([2.9300, 101.6600], { icon: icon }).addTo(map);
+        L.marker([2.9100, 101.6500], { icon: icon }).addTo(map);
 
         return () => {
             map.remove();
